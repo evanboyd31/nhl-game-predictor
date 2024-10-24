628de08d1146ade2cb9a9071d71ae1cd4c5af394
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-10-23 23:17:20 -0700

    Create TeamData model class

--- nhl-game-predictor-backend/games/models.py
+++ nhl-game-predictor-backend/games/models.py
@@ -9,3 +9,39 @@ class Team(models.Model):
     def __str__(self):
         return f"{self.city} ({self.abbreviation})"
 
+class TeamData(models.Model):
+  """
+  Class to records the team's statistics for a provided day for model training
+  """
+  # associated to a team
+  team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team_data')
+
+  # date for which this data was recorded
+  data_capture_date = models.DateField()
+
+  # simple team record states
+  games_played = models.IntegerField()
+  wins = models.IntegerField()
+  losses = models.IntegerField()
+  ot_losses = models.IntegerField()
+  points = models.IntegerField()
+  
+  # team goal statistics
+  goals_for = models.IntegerField()
+  goals_against = models.IntegerField()
+  goal_differential = models.IntegerField()
+
+  # team record stats for the last 10 games
+  l10_games_played = models.IntegerField()
+  l10_wins = models.IntegerField()
+  l10_losses = models.IntegerField()
+
+  # win/loss streak stats
+  streak_count = models.IntegerField()
+  streak_code = models.CharField(max_length=1)  # e.g., W for Win, L for Loss
+
+  class Meta:
+      unique_together = ('team', 'data_capture_date')  # Prevent duplicates for the same team on the same day
+
+  def __str__(self):
+      return f"{self.team} Data ({self.data_capture_date})"
