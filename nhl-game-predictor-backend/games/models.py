8fc06f966db48fc176a276ed8edea5ea356ccb18
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-10-23 23:31:43 -0700

    Create simple model class for an NHL Game

--- nhl-game-predictor-backend/games/models.py
+++ nhl-game-predictor-backend/games/models.py
@@ -38,10 +38,42 @@ class TeamData(models.Model):
 
   # win/loss streak stats
   streak_count = models.IntegerField()
-  streak_code = models.CharField(max_length=1)  # e.g., W for Win, L for Loss
+  streak_code = models.CharField(max_length=1)
 
   class Meta:
-      unique_together = ('team', 'data_capture_date')  # Prevent duplicates for the same team on the same day
+      unique_together = ('team', 'data_capture_date')
 
   def __str__(self):
       return f"{self.team} Data ({self.data_capture_date})"
+    
+class Game(models.Model):
+    """
+    Class to represent a single NHL game
+    """
+    # home and away teams
+    home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_games')
+    away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_games')
+    
+    # date of game
+    game_date = models.DateField()
+    
+    # Game status (e.g., scheduled, completed)
+    status = models.CharField(max_length=20)  # e.g., "Scheduled", "Completed", "Postponed"
+
+    # goals
+    home_team_goals = models.IntegerField(default=0)
+    away_team_goals = models.IntegerField(default=0)
+
+    # shootout and overtime finishes
+    is_overtime = models.BooleanField(default=False)
+    is_shootout = models.BooleanField(default=False)
+
+    # relationships to TeamData for pre-game analysis
+    home_team_data = models.ForeignKey(TeamData, on_delete=models.CASCADE, related_name='home_game_data', null=True)
+    away_team_data = models.ForeignKey(TeamData, on_delete=models.CASCADE, related_name='away_game_data', null=True)
+
+    class Meta:
+        unique_together = ('home_team', 'away_team', 'game_date')
+
+    def __str__(self):
+        return f"{self.home_team} vs {self.away_team} on {self.game_date.strftime('%Y-%m-%d %H:%M')}"
