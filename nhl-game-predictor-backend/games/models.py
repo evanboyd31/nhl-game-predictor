0663cbabd587a1548f6b40bf1804fd8396466d14
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-10-23 22:45:11 -0700

    Create Team model class

--- nhl-game-predictor-backend/games/models.py
+++ nhl-game-predictor-backend/games/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    city = models.CharField(max_length=100)
+    abbreviation = models.CharField(max_length=3, unique=True)
+    logo_url = models.URLField()
+
+    def __str__(self):
+        return f"{self.city} ({self.abbreviation})"
+
