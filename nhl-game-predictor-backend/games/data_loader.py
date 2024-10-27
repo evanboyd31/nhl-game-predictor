eaea8cd83c02b4133965aa5648ebbd814217110f
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-10-27 16:14:43 -0700

    Complete first version of functions to fetch Game and TeamData models for a given team from NHL API

--- nhl-game-predictor-backend/games/data_loader.py
+++ nhl-game-predictor-backend/games/data_loader.py
@@ -5,8 +5,19 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nhl_game_predictor")
 
 from django.conf import settings
-from games.models import Team, Game
-from datetime import datetime
+from games.models import Team, Game, TeamData
+from datetime import datetime, timedelta
+
+# mapping from API-provided strings to integer values for GameData model class
+RESULT_MAP = {
+    "W": TeamData.WIN,
+    "L": TeamData.LOSS,
+    "OT": TeamData.OVERTIME,
+    "FG": TeamData.FIRST_GAME
+}
+
+# reverse results map
+REVERSE_RESULT_MAP = {v: k for k, v in RESULT_MAP.items()}
 
 def load_teams_data_from_api():
     """
@@ -47,6 +58,95 @@ def load_teams_data_from_api():
     # bulk create all new teams identified in above for loop
     Team.objects.bulk_create(teams)
 
+def create_or_update_team_data(team_standings, team, date):
+    # get all remaining statistics from the json response
+    games_played = team_standings.get("gamesPlayed")
+    wins = team_standings.get("wins")
+    losses = team_standings.get("losses")
+    ot_losses = team_standings.get("otLosses")
+    points = team_standings.get("points")
+    goals_for = team_standings.get("goalFor")
+    goals_against = team_standings.get("goalAgainst")
+    goal_differential = team_standings.get("goalDifferential")
+    l10_games_played = team_standings.get("l10GamesPlayed")
+    l10_wins = team_standings.get("l10Wins")
+    l10_losses = team_standings.get("l10Losses")
+    streak_count = team_standings.get("streakCount", 0)
+    streak_code = RESULT_MAP.get(team_standings.get("streakCode", "FG"))
+    team_data = TeamData(team=team,
+                            data_capture_date=date,
+                            games_played=games_played,
+                            wins=wins,
+                            losses=losses,
+                            ot_losses=ot_losses,
+                            points=points,
+                            goals_for=goals_for,
+                            goals_against=goals_against,
+                            goal_differential=goal_differential,
+                            l10_games_played=l10_games_played,
+                            l10_wins=l10_wins,
+                            l10_losses=l10_losses,
+                            streak_count=streak_count,
+                            streak_code=streak_code)
+    
+    team_data, _ = TeamData.objects.update_or_create(
+        team=team,
+        data_capture_date=date,
+        defaults={
+            'games_played': games_played,
+            'wins': wins,
+            'losses': losses,
+            'ot_losses': ot_losses,
+            'points': points,
+            'goals_for': goals_for,
+            'goals_against': goals_against,
+            'goal_differential': goal_differential,
+            'l10_games_played': l10_games_played,
+            'l10_wins': l10_wins,
+            'l10_losses': l10_losses,
+            'streak_count': streak_count,
+            'streak_code': streak_code,
+        }
+    )
+
+    return team_data
+
+def load_team_data_for_date_from_api(team_abbreviation, game_date):
+
+    # ensure team exists
+    team = Team.objects.filter(abbreviation=team_abbreviation).first()
+    if team is None:
+        return
+
+    # get the date before a game
+    previous_day = game_date - timedelta(days=1)
+
+    # check to see if we already have the data for the given team and date
+    previous_team_data = TeamData.objects.filter(team__abbreviation=team_abbreviation, data_capture_date=previous_day).first()
+
+
+    # format the standings URL for the given date
+    formatted_date = previous_day.strftime("%Y-%m-%d")
+    standings_url = f"{settings.NHL_API_BASE_URL}standings/{formatted_date}"
+
+    standings_response = httpx.get(standings_url)
+    standings_json = standings_response.json().get("standings", [])
+
+    if len(standings_json) != 0:
+        for team_standings in standings_json:
+            if team_standings.get("teamAbbrev", {}).get("default", "") == team_abbreviation:
+                team_data = create_or_update_team_data(team_standings=team_standings,
+                                                       team=team,
+                                                       date=previous_day)
+                return team_data
+    else:
+        # if standings is blank, it is the first game of the season, so only supply date and team
+        # all other fields will default to zero
+        team_data = TeamData(team=team,
+                             data_capture_date=previous_day)
+        team_data.save()
+
+        return team_data
 
 def load_games_for_team_from_api(team_abbreviation, past_seasons=0):
     """
@@ -93,64 +193,86 @@ def load_games_for_team_from_api(team_abbreviation, past_seasons=0):
     games_to_update = []
 
     # used to see the completion status of a game
-    current_date = datetime.now()
+    current_date = datetime.now().date()
 
     for season in seasons:
         # get schedule and all games for this season
-        season_schedule_url = f"{settings.NHL_BASE_API_URL}club-schedule-season/{team_abbreviation}/{season}"
-        season_schedule_json = httpx.get(season_schedule_url)
+        season_schedule_url = f"{settings.NHL_API_BASE_URL}club-schedule-season/{team_abbreviation}/{season}"
+        season_schedule_response = httpx.get(season_schedule_url)
+        season_schedule_json = season_schedule_response.json()
         games_json = season_schedule_json.get("games", [])
 
         # iterate over all games
         for game_json in games_json:
-            game_id = games_json.get("id")
-            game_type = games_json.get("gameType")
-            game_date = datetime.strp(game_json.get("gameDate"), "%Y-%m-%d").date()
+            game_id = game_json.get("id")
+            game_type = game_json.get("gameType")
 
-            away_team_json = game_json.get("awayTeam", {})
-            away_team_abbreviation = away_team_json.get("abbrev")
-            away_team = Team.objects.filter(abbreviation=away_team_abbreviation).first()
+            # only create game data when it is not preseason game
+            if game_type != Game.PRESEASON:
+                game_date = datetime.strptime(game_json.get("gameDate"), "%Y-%m-%d").date()
 
-            home_team_json = game_json.get("homeTeam", {})
-            home_team_abbreviation = home_team_json.get("abbrev")
-            home_team = Team.objects.filter(abbreviation=home_team_abbreviation).first()
+                away_team_json = game_json.get("awayTeam", {})
+                away_team_abbreviation = away_team_json.get("abbrev")
+                away_team = Team.objects.filter(abbreviation=away_team_abbreviation).first()
 
-            # store info about game results if it is completed
-            status = games_json.get("gameState")
-            
-            home_team_goals = 0
-            away_team_goals = 0
-            is_overtime = False
-            is_shootout = False
-            if game_date < current_date:
-                home_team_goals = home_team_json.get("score")
-                away_team_goals = away_team_json.get("score")
-                is_overtime = game_json.get("gameOutcome", {}).get("lastPeriodType", "REG") == "OT"
-                is_shootout = game_json.get("gameOutcome", {}).get("lastPeriodType", "REG") == "SO"
-            
+                home_team_json = game_json.get("homeTeam", {})
+                home_team_abbreviation = home_team_json.get("abbrev")
+                home_team = Team.objects.filter(abbreviation=home_team_abbreviation).first()
 
-                winning_team = home_team if home_team_goals > away_team_goals else away_team
-            
-            
-            game = Game(id=id,
-                        home_team=home_team,
-                        away_team=away_team,
-                        winning_team=winning_team,
-                        game_date=game_date,
-                        status=status,
-                        game_type=game_type,
-                        home_team_goals=home_team_goals,
-                        away_team_goals=away_team_goals,
-                        is_overtime=is_overtime,
-                        is_shootout=is_shootout)
-            
-            # if no game already exists, add it to the bulk_create list, otherwise add it to bulk_update list
-            if Game.objects.filter(id=id).count() == 0:
-                games_to_create.append(game)
-            else:
-                games_to_update.append(game)
+                # fields for when the game has been completed
+                home_team_goals = 0
+                away_team_goals = 0
+                is_overtime = False
+                is_shootout = False
+                winning_team = None
+                home_team_data = None
+                away_team_data = None
+                if game_date < current_date:
+                    home_team_goals = home_team_json.get("score")
+                    away_team_goals = away_team_json.get("score")
+                    is_overtime = game_json.get("gameOutcome", {}).get("lastPeriodType", "REG") == "OT"
+                    is_shootout = game_json.get("gameOutcome", {}).get("lastPeriodType", "REG") == "SO"
+
+                    winning_team = home_team if home_team_goals > away_team_goals else away_team
+                    home_team_data = load_team_data_for_date_from_api(team_abbreviation=home_team_abbreviation,
+                                                                    game_date=game_date)
+                    away_team_data = load_team_data_for_date_from_api(team_abbreviation=away_team_abbreviation,
+                                                                    game_date=game_date)
+                game = Game(id=game_id,
+                            home_team=home_team,
+                            away_team=away_team,
+                            winning_team=winning_team,
+                            game_date=game_date,
+                            game_type=game_type,
+                            home_team_goals=home_team_goals,
+                            away_team_goals=away_team_goals,
+                            is_overtime=is_overtime,
+                            is_shootout=is_shootout,
+                            home_team_data=home_team_data,
+                            away_team_data=away_team_data)
+                
+                # if no game already exists, add it to the bulk_create list, otherwise add it to bulk_update list
+                if Game.objects.filter(id=game_id).count() == 0:
+                    games_to_create.append(game)
+                else:
+                    games_to_update.append(game)
 
     # bulk create and update the respective games
     Game.objects.bulk_create(games_to_create)
-    Game.objects.bulk_update(games_to_update)
+    Game.objects.bulk_update(games_to_update, fields=[
+        'winning_team',
+        'home_team_goals',
+        'away_team_goals',
+        'is_overtime',
+        'is_shootout',
+        'game_type',
+        'game_date',
+        'home_team_data',
+        'away_team_data'
+    ])
+
+def clear_database():
+    Team.objects.all().delete()
+    Game.objects.all().delete()
+    TeamData.objects.all().delete()
 
