e2166ec043ebd56680e8ce8948a70e88115cacca
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-10-26 22:39:35 -0700

    Rename city field to team name and remove id for Team model class

--- nhl-game-predictor-backend/games/models.py
+++ nhl-game-predictor-backend/games/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
 class Team(models.Model):
-    id = models.PositiveIntegerField(primary_key=True)
-    city = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     abbreviation = models.CharField(max_length=3, unique=True)
     logo_url = models.URLField()
 
     def __str__(self):
-        return f"{self.city} ({self.abbreviation})"
+        return f"{self.name} ({self.abbreviation})"
 
 class TeamData(models.Model):
   """
