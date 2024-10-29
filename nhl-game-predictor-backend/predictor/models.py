91d949bc1f311fa908013abc700c8424fad4346e
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-10-29 13:35:51 -0700

    Create PredictionModel class

--- nhl-game-predictor-backend/predictor/models.py
+++ nhl-game-predictor-backend/predictor/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class PredictionModel(models.Model):
+    """
+    class to represent a machine learning model used for making game predictions
+    """
+    name = models.CharField(max_length=100, unique=True)
+    version = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        # eg: Random Forest (v.2.3)
+        return f"{self.name} (Version: {self.version})"
