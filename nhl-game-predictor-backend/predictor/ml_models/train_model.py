1cea2861415f346f6f14df689cb4f21076dc6288
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-10-29 17:59:01 -0700

    Update train random forest function to create a prediction model after training

--- nhl-game-predictor-backend/predictor/ml_models/train_model.py
+++ nhl-game-predictor-backend/predictor/ml_models/train_model.py
@@ -1,6 +1,9 @@
 import os
 import django
 import pandas as pd
+from django.db import transaction
+from django.db.models import Max
+from predictor.models import PredictionModel
 from games.models import Game
 from datetime import datetime
 import pickle
@@ -8,148 +11,10 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from predictor.ml_models.utils import GameDataFrameEntry
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nhl_game_predictor_backend")
 django.setup()
-
-class GameDataFrameEntry:
-    def __init__(self, game: Game):
-        game_json = game.game_json
-        home_team_data = game.home_team_data
-        home_team_data_json = home_team_data.team_data_json
-        away_team_data = game.away_team_data
-        away_team_data_json = away_team_data.team_data_json
-        
-        # general game data
-        self.home_team = game.home_team.franchise.pk
-        self.away_team = game.away_team.franchise.pk
-        self.game_type = game_json.get("gameTypeId", 1)
-
-        self.game_month = game.game_date.month
-        self.game_day_of_week = game.game_date.weekday()
-
-        # home team data (using rates)
-        home_team_games_played = home_team_data_json.get("gamesPlayed", 1) if home_team_data_json.get("gamesPlayed", 1) != 0 else 1
-        self.home_team_win_percentage = home_team_data_json.get("wins", 0) / home_team_games_played
-        self.home_team_loss_percentage = home_team_data_json.get("losses", 0) / home_team_games_played
-        self.home_team_ot_loss_percentage = home_team_data_json.get("otLosses", 0) / home_team_games_played
-        self.home_team_goals_for_per_game = home_team_data_json.get("goalFor", 0) / home_team_games_played
-        self.home_team_goals_against_per_game = home_team_data_json.get("goalAgainst", 0) / home_team_games_played
-        self.home_team_goal_differential_per_game = home_team_data_json.get("goalDifferential", 0) / home_team_games_played
-
-
-        home_team_l10_games_played = home_team_data_json.get("l10GamesPlayed", 1) if game.home_team_data.team_data_json.get("l10GamesPlayed") != 0 else 1
-        self.home_team_l10_win_percentage = home_team_data_json.get("l10Wins", 0) / home_team_l10_games_played
-        self.home_team_l10_loss_percentage = home_team_data_json.get("l10Losses", 0) / home_team_l10_games_played
-        self.home_team_l10_ot_losses = home_team_data_json.get("l10OtLosses", 0) / home_team_l10_games_played
-        self.home_team_l10_goals_for_per_game = home_team_data_json.get("l10GoalsFor", 0) / home_team_l10_games_played
-        self.home_team_l10_goals_against_per_game = home_team_data_json.get("l10GoalsAgainst", 0) / home_team_l10_games_played
-        self.home_team_l10_goal_differential_per_game = home_team_data_json.get("l10GoalDifferential", 0) / home_team_l10_games_played
-
-        # home team home data
-        home_games_played = home_team_data_json.get("homeGamesPlayed", 1) if home_team_data_json.get("homeGamesPlayed", 1) != 0 else 1
-        self.home_team_home_win_percentage = home_team_data_json.get("homeWins", 0) / home_games_played 
-        self.home_team_home_loss_percentage = home_team_data_json.get("homeLosses", 0) / home_games_played 
-        self.home_team_home_ot_loss_percentage = home_team_data_json.get("homeOtLosses", 0) / home_games_played 
-        self.home_team_home_goals_for_per_game = home_team_data_json.get("homeGoalsFor", 0) / home_games_played 
-        self.home_team_home_goals_against_per_game = home_team_data_json.get("homeGoalsAgainst", 0) / home_games_played 
-        self.home_team_home_goal_differential_per_game = home_team_data_json.get("homeGoalDifferent", 0) / home_games_played
-
-
-        # away team data (using rates)
-        away_team_games_played = away_team_data_json.get("gamesPlayed", 1) if away_team_data_json.get("gamesPlayed", 1) != 0 else 1
-        self.away_team_win_percentage = away_team_data_json.get("wins", 0) / away_team_games_played 
-        self.away_team_loss_percentage = away_team_data_json.get("losses", 0) / away_team_games_played 
-        self.away_team_ot_loss_percentage = away_team_data_json.get("otLosses", 0) / away_team_games_played 
-        self.away_team_goals_for_per_game = away_team_data_json.get("goalFor", 0) / away_team_games_played 
-        self.away_team_goals_against_per_game = away_team_data_json.get("goalAgainst", 0) / away_team_games_played 
-        self.away_team_goal_differential_per_game = away_team_data_json.get("goalDifferential", 0) / away_team_games_played 
-
-
-        away_team_l10_games_played = away_team_data_json.get("l10GamesPlayed", 1) if away_team_data_json.get("l10GamesPlayed") != 0 else 1
-        self.away_team_l10_win_percentage = away_team_data_json.get("l10Wins", 0) / away_team_l10_games_played
-        self.away_team_l10_loss_percentage = away_team_data_json.get("l10Losses", 0) / away_team_l10_games_played
-        self.away_team_l10_ot_losses = away_team_data_json.get("l10OtLosses", 0) / away_team_l10_games_played
-        self.away_team_l10_goals_for_per_game = away_team_data_json.get("l10GoalsFor", 0) / away_team_l10_games_played
-        self.away_team_l10_goals_against_per_game = away_team_data_json.get("l10GoalsAgainst", 0) / away_team_l10_games_played
-        self.away_team_l10_goal_differential_per_game = away_team_data_json.get("l10GoalDifferential", 0) / away_team_l10_games_played
-
-        # away team road data
-        away_games_played = away_team_data_json.get("roadGamesPlayed", 1) if away_team_data_json.get("roadGamesPlayed", 1) != 0 else 1
-        self.away_team_road_win_percentage = away_team_data_json.get("roadWins", 0) / away_games_played 
-        self.away_team_road_loss_percentage = away_team_data_json.get("roadLosses", 0) / away_games_played 
-        self.away_team_road_ot_loss_percentage = away_team_data_json.get("roadOtLosses", 0) / away_games_played 
-        self.away_team_road_goals_for_per_game = away_team_data_json.get("roadGoalsFor", 0) / away_games_played 
-        self.away_team_road_goals_against_per_game = away_team_data_json.get("roadGoalsAgainst", 0) / away_games_played 
-        self.away_team_road_goal_differential_per_game = away_team_data_json.get("roadGoalDifferential", 0) / away_games_played 
-
-        # labels
-        self.home_team_win = 1 if game.winning_team == game.home_team else 0
-
-
-    def to_dict(self):
-        """
-        Convert the instance to a dictionary for easy DataFrame creation.
-        """
-        return {
-            "home_team": self.home_team,
-            "away_team": self.away_team,
-            "game_type": self.game_type,
-            "game_month": self.game_month,
-            "game_day_of_week": self.game_day_of_week,
-            # Home team overall percentage metrics
-            "home_team_win_percentage": self.home_team_win_percentage,
-            "home_team_loss_percentage": self.home_team_loss_percentage,
-            "home_team_ot_loss_percentage": self.home_team_ot_loss_percentage,
-            "home_team_goals_for_per_game": self.home_team_goals_for_per_game,
-            "home_team_goals_against_per_game": self.home_team_goals_against_per_game,
-            "home_team_goal_differential_per_game": self.home_team_goal_differential_per_game,
-            # Home team last 10 games data
-            "home_team_l10_win_percentage": self.home_team_l10_win_percentage,
-            "home_team_l10_loss_percentage": self.home_team_l10_loss_percentage,
-            "home_team_l10_ot_losses": self.home_team_l10_ot_losses,
-            "home_team_l10_goals_for_per_game": self.home_team_l10_goals_for_per_game,
-            "home_team_l10_goals_against_per_game": self.home_team_l10_goals_against_per_game,
-            "home_team_l10_goal_differential_per_game": self.home_team_l10_goal_differential_per_game,
-
-            # Home team home data
-            "home_team_home_win_percentage": self.home_team_home_win_percentage,
-            "home_team_home_loss_percentage": self.home_team_home_loss_percentage,
-            "home_team_home_ot_loss_percentage": self.home_team_home_ot_loss_percentage,
-            "home_team_home_goals_for_per_game": self.home_team_home_goals_for_per_game,
-            "home_team_home_goals_against_per_game": self.home_team_home_goals_against_per_game,
-            "home_team_home_goal_differential_per_game": self.home_team_home_goal_differential_per_game,
-
-            # Away team overall percentage metrics
-            "away_team_win_percentage": self.away_team_win_percentage,
-            "away_team_loss_percentage": self.away_team_loss_percentage,
-            "away_team_ot_loss_percentage": self.away_team_ot_loss_percentage,
-            "away_team_goals_for_per_game": self.away_team_goals_for_per_game,
-            "away_team_goals_against_per_game": self.away_team_goals_against_per_game,
-            "away_team_goal_differential_per_game": self.away_team_goal_differential_per_game,
-            "away_team_l10_win_percentage": self.away_team_l10_win_percentage,
-            "away_team_l10_loss_percentage": self.away_team_l10_loss_percentage,
-            "away_team_l10_ot_losses": self.away_team_l10_ot_losses,
-            "away_team_l10_goals_for_per_game": self.away_team_l10_goals_for_per_game,
-            "away_team_l10_goals_against_per_game": self.away_team_l10_goals_against_per_game,
-            "away_team_l10_goal_differential_per_game": self.away_team_l10_goal_differential_per_game,
-
-            # Away team road data
-            "away_team_road_win_percentage": self.away_team_road_win_percentage,
-
-            # Away team road data
-            "away_team_road_win_percentage": self.away_team_road_win_percentage,
-            "away_team_road_loss_percentage": self.away_team_road_loss_percentage,
-            "away_team_road_ot_loss_percentage": self.away_team_road_ot_loss_percentage,
-            "away_team_road_goals_for_per_game": self.away_team_road_goals_for_per_game,
-            "away_team_road_goals_against_per_game": self.away_team_road_goals_against_per_game,
-            "away_team_road_goal_differential_per_game": self.away_team_road_goal_differential_per_game,
-
-            # label is a simple binary decision of did the home team win or not
-            "home_team_win": self.home_team_win,
-        }
-
-
 
 def generate_past_season_ids(past_seasons):
     current_date = datetime.now()
@@ -186,6 +51,7 @@ def create_data_frame(past_seasons):
 
     return game_data_df
 
+@transaction.atomic
 def train_random_forest(past_seasons):
     game_data_df = create_data_frame(past_seasons=past_seasons)
 
@@ -207,11 +73,20 @@ def train_random_forest(past_seasons):
 
     predicted_labels = random_forest.predict(testing_features)
 
-    # Evaluate the model
+    # evaluate accuracy
     accuracy = accuracy_score(testing_labels, predicted_labels)
     print("accuracy:", accuracy)
 
-    with open('./predictor/ml_models/trained_models/random_forest_model.pkl', 'wb') as file:
+    # create a new prediction model object and save
+    last_version = PredictionModel.objects.aggregate(Max('version'))['version__max']
+    if last_version:
+        major, minor = map(int, last_version.split('.'))
+        new_version = f"{major}.{minor + 1}"
+    else:
+        new_version = "1.0"
+
+    prediction_model = PredictionModel.objects.create(name="Random Forest", version=new_version)
+    with open(f'./predictor/ml_models/trained_models/random-forest-v-{new_version.replace(".","-")}.pkl', 'wb') as file:
         pickle.dump(random_forest, file)
 
-    print("Model saved successfully.")
+    print(f"Model Random Forest (v{new_version}) saved successfully.")
