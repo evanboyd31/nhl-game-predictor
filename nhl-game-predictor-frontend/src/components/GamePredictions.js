3eda6d96a968225b327bd32f58f37c9c4f10aae9
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-11-02 09:44:04 -0700

    Change style of Game Predictions title header in page body

--- nhl-game-predictor-frontend/src/components/GamePredictions.js
+++ nhl-game-predictor-frontend/src/components/GamePredictions.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "../styles/game-predictions.css";
+import GamePrediction from "./GamePrediction";
+
+const formatDate = (date) => {
+  const options = { month: "long", day: "numeric", year: "numeric" };
+  const day = date.getDate();
+  const suffix = day === 1 ? "st" : day === 2 ? "nd" : day === 3 ? "rd" : "th";
+  return `${date
+    .toLocaleString("default", options)
+    .replace(day, day + suffix)}`;
+};
 
 const GamePredictions = () => {
-  return <ul></ul>;
+  return (
+    <div className="game-predictions">
+      <h3 className="game-predictions-header">
+        Predictions for games on {formatDate(new Date())}
+      </h3>
+      <ul className="game-predictions-list">
+        <GamePrediction />
+      </ul>
+    </div>
+  );
 };
 
 export default GamePredictions;
