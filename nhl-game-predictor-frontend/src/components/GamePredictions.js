2b0d9de51dbcc2b6d7dc88405eff10e69567cb3a
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-11-02 12:26:03 -0700

    Reformat predictions title and add prompt for user to click on games

--- nhl-game-predictor-frontend/src/components/GamePredictions.js
+++ nhl-game-predictor-frontend/src/components/GamePredictions.js
@@ -4,16 +4,8 @@ import "../styles/game-predictions.css";
 import GamePrediction from "./GamePrediction";
 import gamePredictions from "./TestData";
 
-const formatDate = (date) => {
-  const options = { month: "long", day: "numeric", year: "numeric" };
-  const day = date.getDate();
-  const suffix = day === 1 ? "st" : day === 2 ? "nd" : day === 3 ? "rd" : "th";
-  return `${date
-    .toLocaleString("default", options)
-    .replace(day, day + suffix)}`;
-};
-
 const GamePredictions = () => {
+  const today = new Date();
   const [isOpen, setIsOpen] = useState(
     gamePredictions.map((gamePrediction) => ({
       id: gamePrediction.id,
@@ -34,8 +26,9 @@ const GamePredictions = () => {
   return (
     <div className="game-predictions">
       <h3 className="game-predictions-header">
-        Predictions for games on {formatDate(new Date())}
+        Predictions for {today.toLocaleDateString()}
       </h3>
+      <h4>Click on a game to see predictions!</h4>
       <ul className="game-predictions-list">
         {gamePredictions.map((gamePrediction) => {
           const isOpenPrediction = isOpen.find(
