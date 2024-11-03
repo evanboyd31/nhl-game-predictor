6bc704b4ce35415073a16f0c21dc694595990667
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-11-02 19:06:05 -0700

    Add production frontend domain to allowed cors origin

--- nhl-game-predictor-backend/nhl_game_predictor/settings.py
+++ nhl-game-predictor-backend/nhl_game_predictor/settings.py
@@ -43,6 +43,7 @@ CSRF_TRUSTED_ORIGINS = ['https://nhl-game-predictor-backend.onrender.com',
                         'http://localhost:3000']
 
 CORS_ALLOWED_ORIGINS = [
+    'https://nhl-game-predictor.onrender.com',
     "http://localhost:3000",
     "http://127.0.0.1:3000", 
 
