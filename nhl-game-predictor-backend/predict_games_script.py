41ce1a16fedfb8f6bd3988ccda835acf4a422a8c
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-11-04 17:03:20 -0500

    Add PREDICT-GAMES-TODAY-TOKEN to the predict_games_script script

--- nhl-game-predictor-backend/predict_games_script.py
+++ nhl-game-predictor-backend/predict_games_script.py
@@ -1,12 +1,15 @@
 import httpx
 import time
+import os
 
 URL = "https://nhl-game-predictor-backend.onrender.com/api/predict-games-today/"
+PREDICT_GAMES_TODAY_ACCESS_TOKEN = os.getenv("PREDICT_GAMES_TODAY_ACCESS_TOKEN", "")
 
 def predict_games(number_of_seconds=60*30):
+    headers = {"PREDICT-GAMES-TODAY-TOKEN": PREDICT_GAMES_TODAY_ACCESS_TOKEN}
     while True:
         try:
-            response = httpx.get(url=URL, timeout=number_of_seconds) 
+            response = httpx.get(url=URL, headers=headers, timeout=number_of_seconds) 
             if response.status_code == 200:
                 print(f"Games have been predicted")
             else:
