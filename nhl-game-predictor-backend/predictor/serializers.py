e1dd66131b5fa39cf2323f70639ea00bf086821d
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-11-06 21:11:46 -0500

    Create docstring for PredictionModelSerializer

--- nhl-game-predictor-backend/predictor/serializers.py
+++ nhl-game-predictor-backend/predictor/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import PredictionModel
 
 class PredictionModelSerializer(serializers.ModelSerializer):
+    """
+    serializes all fields of a PredictionModel model instance into JSON
+    """
+    
     class Meta:
         model = PredictionModel
         fields = '__all__'
