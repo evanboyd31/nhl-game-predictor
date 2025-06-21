dfdf2e079f706069b7a3a362eb03691bb97843b4
Evan Boyd <evan.boyd@mail.mcgill.ca>
2025-06-20 21:01:19 -0400

    Use ServerPinger class in keep_servers_active.py script

--- nhl-game-predictor-backend/keep_servers_active.py
+++ nhl-game-predictor-backend/keep_servers_active.py
@@ -1,28 +1,9 @@
-import httpx
-import os
-from dotenv import load_dotenv
-load_dotenv()
+from server_pinger import ServerPinger
 
-URL = "https://nhl-game-predictor-backend.onrender.com/api/keep-active/"
-KEEP_ACTIVE_ACCESS_TOKEN = os.getenv("KEEP_ACTIVE_ACCESS_TOKEN", "")
+pinger = ServerPinger(
+    url="https://nhl-game-predictor-backend.onrender.com/api/keep-active/",
+    token_env_var="KEEP_ACTIVE_ACCESS_TOKEN",
+    header_name="KEEP-ACTIVE-TOKEN"
+)
 
-def keep_servers_active():
-    """
-    function to ping the KeepActiveView REST API endpoint to keep
-    both Render and Supabase instance active
-    """
-    headers = {"KEEP-ACTIVE-TOKEN": KEEP_ACTIVE_ACCESS_TOKEN}
-    print(KEEP_ACTIVE_ACCESS_TOKEN)
-
-    try:
-        response = httpx.get(url=URL, 
-                             headers=headers) 
-        if response.status_code == 200:
-            print(f"Pinged {URL}, server is active.")
-        else:
-            print(f"Pinged {URL}, received non-200 status code: {response.status_code}")
-    except httpx.RequestError as e:
-        print(f"Error pinging {URL}: {e}")
-    
-
-keep_servers_active()
+pinger.ping()
