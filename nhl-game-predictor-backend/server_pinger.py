845ed064bf9f789b5ddb8c7631c363fd0a14b489
Evan Boyd <evan.boyd@mail.mcgill.ca>
2025-06-20 21:11:33 -0400

    Add query params to ServerPinger constructor and requests

--- nhl-game-predictor-backend/server_pinger.py
+++ nhl-game-predictor-backend/server_pinger.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class ServerPinger:
-  def __init__(self, url: str, token_env_var: str, header_name: str):
+  def __init__(self, url: str, token_env_var: str, header_name: str, params: dict = None):
     self.url = url
     self.token = os.getenv(token_env_var, "")
     self.header_name = header_name
+    self.params = params or {}
 
     if not self.token:
       raise ValueError(f"Missing token for {token_env_var}")
@@ -16,11 +17,11 @@ class ServerPinger:
   def ping(self):
     headers = {self.header_name: self.token}
     try:
-      response = httpx.get(self.url, headers=headers)
+      response = httpx.get(self.url, headers=headers, params=self.params)
       if response.status_code == 200:
-        print(f"Successfully pinged {self.url}")
+        print(f"Successfully pinged {response.url}")
       else:
-        print(f"Pinged {self.url}, got status code: {response.status_code}")
+        print(f"Pinged {response.url}, got status code: {response.status_code}")
+        print(f"Response details: {response.text}")
     except httpx.RequestError as e:
       print(f"Error pinging {self.url}: {e}")
-
