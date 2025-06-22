a1ffdaee8eaa6bc582cbf707540658bd7750d782
Evan Boyd <evan.boyd@mail.mcgill.ca>
2025-06-22 09:53:30 -0400

    Confirm local and prod database environment variables are accessible in push_local_to_prod command

---
+++ nhl-game-predictor-backend/games/management/commands/push_local_to_prod.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+  help = "Restores the production Supabase instance with your local database"
+
+  def handle(self, *args, **options):
+    local_db_name = os.getenv("DATABASE_NAME")
+    local_db_user = os.getenv("DATABASE_USER") 
+    local_db_password = os.getenv("DATABASE_PASSWORD") 
+    local_db_host = os.getenv("DATABASE_HOST") 
+    local_db_port = os.getenv("DATABASE_PORT") 
+    prod_db_name = os.getenv("PROD_DATABASE_NAME") 
+    prod_db_user = os.getenv("PROD_DATABASE_USER") 
+    prod_db_password = os.getenv("PROD_DATABASE_PASSWORD") 
+    prod_db_host = os.getenv("PROD_DATABASE_HOST") 
+    prod_db_port = os.getenv("PROD_DATABASE_PORT")
