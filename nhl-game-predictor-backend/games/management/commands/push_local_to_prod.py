7075d55c48df75aeaad142cad282bec6126f4341
Evan Boyd <evan.boyd@mail.mcgill.ca>
2025-06-22 09:54:51 -0400

    Add comments and space out env variables

--- nhl-game-predictor-backend/games/management/commands/push_local_to_prod.py
+++ nhl-game-predictor-backend/games/management/commands/push_local_to_prod.py
@@ -6,13 +6,16 @@ class Command(BaseCommand):
   help = "Restores the production Supabase instance with your local database"
 
   def handle(self, *args, **options):
+    # credentials needed to access local database
     local_db_name = os.getenv("DATABASE_NAME")
-    local_db_user = os.getenv("DATABASE_USER") 
-    local_db_password = os.getenv("DATABASE_PASSWORD") 
-    local_db_host = os.getenv("DATABASE_HOST") 
-    local_db_port = os.getenv("DATABASE_PORT") 
-    prod_db_name = os.getenv("PROD_DATABASE_NAME") 
-    prod_db_user = os.getenv("PROD_DATABASE_USER") 
-    prod_db_password = os.getenv("PROD_DATABASE_PASSWORD") 
-    prod_db_host = os.getenv("PROD_DATABASE_HOST") 
+    local_db_user = os.getenv("DATABASE_USER")
+    local_db_password = os.getenv("DATABASE_PASSWORD")
+    local_db_host = os.getenv("DATABASE_HOST")
+    local_db_port = os.getenv("DATABASE_PORT")
+
+    # credentials needed to access production supabase-hosted database
+    prod_db_name = os.getenv("PROD_DATABASE_NAME")
+    prod_db_user = os.getenv("PROD_DATABASE_USER")
+    prod_db_password = os.getenv("PROD_DATABASE_PASSWORD")
+    prod_db_host = os.getenv("PROD_DATABASE_HOST")
     prod_db_port = os.getenv("PROD_DATABASE_PORT")
