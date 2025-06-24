b08b325d8f0d5f5d24ed5cd8f31a53e31bb231e2
Evan Boyd <evan.boyd@mail.mcgill.ca>
2025-06-24 10:25:14 -0400

    Add confirmation input, error handling of env vars, and command execution of local db dumping

--- nhl-game-predictor-backend/games/management/commands/push_local_to_prod.py
+++ nhl-game-predictor-backend/games/management/commands/push_local_to_prod.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
 
 class Command(BaseCommand):
   help = "Restores the production Supabase instance with your local database"
@@ -19,3 +20,36 @@ class Command(BaseCommand):
     prod_db_password = os.getenv("PROD_DATABASE_PASSWORD")
     prod_db_host = os.getenv("PROD_DATABASE_HOST")
     prod_db_port = os.getenv("PROD_DATABASE_PORT")
+
+    if not all([local_db_name, local_db_user, local_db_password, local_db_host, local_db_port]):
+      raise CommandError("Missing production DB credentials. Check your env vars.")
+
+    if not all([prod_db_name, prod_db_user, prod_db_password, prod_db_host, prod_db_port]):
+      raise CommandError("Missing production DB credentials. Check your env vars.")
+    
+    confirm = input(
+        "\033[93mWARNING: This will overwrite the PROD database. Are you sure? [yes/no]: \033[0m"
+    )
+    if confirm.lower() != "yes":
+      self.stdout.write("Aborting.")
+      return
+
+    # construct the file path for the local dump
+    now = timezone.now().strftime("%Y%m%d_%H%M%S")
+    command_dir = os.path.dirname(os.path.abspath(__file__))
+    dump_dir = os.path.join(command_dir, "local_db_dumps")
+    dump_file_path = f"{dump_dir}/local_dump_{now}.sql"
+    os.makedirs(dump_dir, exist_ok=True)
+
+    # dump local DB
+    self.stdout.write("Dumping local DB...")
+    subprocess.run([
+      'pg_dump',
+      '-h', local_db_host,
+      '-p', local_db_port,
+      '-U', local_db_user,
+      '-d', local_db_name,
+      '-f', dump_file_path
+    ], check=True, env={"PGPASSWORD": local_db_password})
+
+    self.stdout.write(f"Dumped local DB into `{dump_file_path}`.")
