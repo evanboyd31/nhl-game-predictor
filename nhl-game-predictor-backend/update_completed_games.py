49129bea3fa9152947a0f04eba905db374585c87
Evan Boyd <evan.boyd@mail.mcgill.ca>
2025-06-29 13:15:54 -0400

    Fix update_completed_games url to point to prod

--- nhl-game-predictor-backend/update_completed_games.py
+++ nhl-game-predictor-backend/update_completed_games.py
@@ -1,7 +1,7 @@
 from server_pinger import ServerPinger
 
 pinger = ServerPinger(
-    url="http://localhost:8001/api/update-completed-games/",
+    url="https://nhl-game-predictor-backend.onrender.com/api/update-completed-games/",
     token_env_var="UPDATE_COMPLETED_GAMES_TOKEN",
     header_name="UPDATE-COMPLETED-GAMES-TOKEN"
 )
