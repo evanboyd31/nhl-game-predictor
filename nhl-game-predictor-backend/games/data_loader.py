fb4c61e8cc369bb07dbf7fdc98109a59f1663418
Evan Boyd <evan.boyd@mail.mcgill.ca>
2025-07-01 13:46:10 -0400

    Fix comments

--- nhl-game-predictor-backend/games/data_loader.py
+++ nhl-game-predictor-backend/games/data_loader.py
@@ -24,7 +24,6 @@ REVERSE_RESULT_MAP = {v: k for k, v in RESULT_MAP.items()}
 
 def convert_game_json_to_game_data_objects(game_json : dict, date_string : str, get_team_data : bool = False):
     game_id = game_json.get("id")
-    game_type = game_json.get("gameType")
     game_date = datetime.strptime(date_string, "%Y-%m-%d").date()
     current_date = timezone.localdate()
 
@@ -33,7 +32,7 @@ def convert_game_json_to_game_data_objects(game_json : dict, date_string : str, 
     home_team_goals = home_team_json.get("score", 0)
     away_team_goals = away_team_json.get("score", 0)
 
-    # only create game data when it is not preseason game and doesn't already exist
+    # only create game data when it doesn't already exist
     game = None
     home_team_data = None
     away_team_data = None
@@ -59,9 +58,11 @@ def convert_game_json_to_game_data_objects(game_json : dict, date_string : str, 
             away_team_data = load_team_data_for_date_from_api(team=away_team,
                                                               game_date=game_date)
             
-
+        
+        # assign game to season model
         season_id = game_json.get("season")
         season = Season.objects.filter(id=season_id).first()
+        
         game = Game(id=game_id,
                     season=season,
                     game_date=game_date,
