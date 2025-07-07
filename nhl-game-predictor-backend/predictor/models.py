5a033d5ff1b3b31b7ce6c8cf522fa7f379d1bc1a
Evan Boyd <evan.boyd@mail.mcgill.ca>
2025-07-07 19:05:26 -0400

    Make name and version unique together for PredictionModel

--- nhl-game-predictor-backend/predictor/models.py
+++ nhl-game-predictor-backend/predictor/models.py
@@ -4,8 +4,11 @@ class PredictionModel(models.Model):
     """
     class to represent a machine learning model used for making game predictions
     """
-    name = models.CharField(max_length=100, unique=True)
-    version = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=100)
+    version = models.CharField(max_length=50)
+
+    class Meta:
+        unique_together = ("name", "version")
 
     def __str__(self):
         # eg: Random Forest (v.2.3)
