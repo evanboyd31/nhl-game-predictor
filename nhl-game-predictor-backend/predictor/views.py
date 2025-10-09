153776cddc10bd4b8fa4717b502e7bef425167fd
Evan Boyd <evan.boyd@mail.mcgill.ca>
2025-10-09 16:14:35 -0400

    Create MostRecentPredictionModelDetailView

--- nhl-game-predictor-backend/predictor/views.py
+++ nhl-game-predictor-backend/predictor/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import PredictionModel
+from .serializers import PredictionModelSerializer
 
-# Create your views here.
+
+class MostRecentPredictionModelDetailView(APIView):
+
+  def get(self, request):
+    latest_prediction_model = PredictionModel.objects.order_by('-version').first()
+    serializer = PredictionModelSerializer(latest_prediction_model)
+
+    return Response(serializer.data)
+  
